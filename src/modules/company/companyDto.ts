4dc1e2b7d2317dc3aea14a7425fafa65272b2aac
Erick-Nu <erick.nunez02@epn.edu.ec>
2026-05-25 09:23:22 -0500

    Feature: Agregar archivo companyDto.ts en la capa dto del modulo company

---
+++ src/modules/company/companyDto.ts
@@ -0,0 +1,60 @@
+import {Status} from '../../config/databaseTypes.js';
+
+
+type CreateCompanyDto = {
+  emruc: string;
+  emrznsocial: string;
+  emcorreo: string;
+  emlogo: string;
+  emcodigo: string;
+};
+
+type FindCompanyDto = {
+  emid: string;
+}
+
+type UpdateStatusCompanyDto = {
+  emid: string;
+  emestado: Status;
+}
+
+type UpdateCompanyDto = {
+  emid: string;
+  emrznsocial?: string;
+  emcorreo?: string;
+  emlogo?: string;
+}
+
+type CompanyResponseDto = {
+  emid: string;
+  emruc: string;
+  emrznsocial: string;
+  emcorreo: string;
+  emlogo: string;
+  emcodigo: string;
+  emfchregistro: Date;
+  emestado: Status;
+}
+
+type FindCompaniesParamsDto = {
+  page: number;
+  pageSize: number;
+};
+
+type FindCompaniesResponseDto = {
+  items: CompanyResponseDto[];
+  page: number;
+  pageSize: number;
+  totalItems: number;
+  totalPages: number;
+};
+
+export type {
+  CreateCompanyDto,
+  FindCompanyDto,
+  UpdateCompanyDto,
+  CompanyResponseDto,
+  FindCompaniesParamsDto,
+  FindCompaniesResponseDto,
+  UpdateStatusCompanyDto
+};
