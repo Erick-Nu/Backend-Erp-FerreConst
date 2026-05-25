4716b65a2bcdd55c2411ea5aacb24c0785942c29
Erick-Nu <erick.nunez02@epn.edu.ec>
2026-05-25 09:23:23 -0500

    Feature: Agregar archivo categoryDto.ts en la capa dto del modulo category

---
+++ src/modules/category/categoryDto.ts
@@ -0,0 +1,49 @@
+import type { Status } from '../../config/databaseTypes.js';
+
+type CreateCategoryDto = {
+  ctgriaemid: string;
+  ctgnombre: string;
+  ctgriadescripcion: string | null;
+};
+
+type FindCategoryDto = {
+  ctgriaid: string;
+};
+
+type UpdateCategoryDto = {
+  ctgriaid: string;
+  ctgnombre?: string;
+  ctgriadescripcion?: string | null;
+  ctgriaestado?: Status;
+};
+
+type FindCategoriesParamsDto = {
+  page: number;
+  pageSize: number;
+};
+
+type CategoryResponseDto = {
+  ctgriaid: string;
+  ctgriaemid: string;
+  ctgnombre: string;
+  ctgriadescripcion: string | null;
+  ctgriafchregistro: Date;
+  ctgriaestado: Status;
+};
+
+type FindCategoriesResponseDto = {
+  items: CategoryResponseDto[];
+  page: number;
+  pageSize: number;
+  totalItems: number;
+  totalPages: number;
+};
+
+export type {
+  CreateCategoryDto,
+  FindCategoryDto,
+  UpdateCategoryDto,
+  FindCategoriesParamsDto,
+  CategoryResponseDto,
+  FindCategoriesResponseDto,
+};
