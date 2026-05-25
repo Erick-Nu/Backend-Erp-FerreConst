4c0a0e35463f50d5113d26b5ffbbd9b670300e4b
Erick-Nu <erick.nunez02@epn.edu.ec>
2026-05-25 09:23:25 -0500

    Feature: Agregar archivo clientDto.ts en la capa dto del modulo client

---
+++ src/modules/client/clientDto.ts
@@ -0,0 +1,61 @@
+import type { Identification, Status } from '../../config/databaseTypes.js';
+
+type CreateClientDto = {
+  clnteemid: string;
+  clntetipoidentificacion: Identification;
+  clnteidentificacion: string;
+  clntenombre: string;
+  clntecorreo: string;
+  clntedireccion: string;
+  clntetelefono: string;
+};
+
+type FindClientDto = {
+  clnteid: string;
+};
+
+type UpdateClientDto = {
+  clnteid: string;
+  clntetipoidentificacion?: Identification;
+  clnteidentificacion?: string;
+  clntenombre?: string;
+  clntecorreo?: string;
+  clntedireccion?: string;
+  clntetelefono?: string;
+  clnteestado?: Status;
+};
+
+type FindClientsParamsDto = {
+  page: number;
+  pageSize: number;
+};
+
+type ClientResponseDto = {
+  clnteid: string;
+  clnteemid: string;
+  clntetipoidentificacion: Identification;
+  clnteidentificacion: string;
+  clntenombre: string;
+  clntecorreo: string;
+  clntedireccion: string;
+  clntetelefono: string;
+  clntefchregistro: Date;
+  clnteestado: Status;
+};
+
+type FindClientsResponseDto = {
+  items: ClientResponseDto[];
+  page: number;
+  pageSize: number;
+  totalItems: number;
+  totalPages: number;
+};
+
+export type {
+  CreateClientDto,
+  FindClientDto,
+  UpdateClientDto,
+  FindClientsParamsDto,
+  ClientResponseDto,
+  FindClientsResponseDto,
+};
