df265ca911d4d97106f8d618b0dab6dfa1a3b33f
Erick-Nu <erick.nunez02@epn.edu.ec>
2026-05-25 09:23:23 -0500

    Feature: Agregar archivo checkoutController.ts en la capa controller del modulo checkout

---
+++ src/modules/checkout/checkoutController.ts
@@ -0,0 +1,115 @@
+import type { RequestHandler } from 'express';
+import type { LoginUserDto } from '../auth/authDto.js';
+import type {
+  CreateCheckoutDto,
+  FindCheckoutDto,
+  FindCheckoutsParamsDto,
+  UpdateCheckoutDto,
+} from './checkoutDto.js';
+import { createCheckout, readCheckout, readCheckouts, updateCheckout } from './checkoutService.js';
+
+type UpdateCheckoutRequestBody = Omit<UpdateCheckoutDto, 'cjid'>;
+
+const registerCheckout: RequestHandler = async (req, res, next) => {
+  try {
+    const { cjemid, cjsuid, cjidentificador } = req.body;
+    const checkout: CreateCheckoutDto = {
+      cjemid,
+      cjsuid,
+      cjidentificador,
+    };
+    const user: LoginUserDto = req.auth!;
+
+    const checkoutDB = await createCheckout(checkout, user);
+
+    res.status(201).json(checkoutDB);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const searchCheckout: RequestHandler = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { cjsuid } = req.query;
+
+    if (typeof id !== 'string') {
+      res.status(400).json({ message: 'Checkout id is required' });
+      return;
+    }
+
+    if (typeof cjsuid !== 'string') {
+      res.status(400).json({ message: 'Branch id is required' });
+      return;
+    }
+
+    const checkout: FindCheckoutDto = {
+      cjid: id,
+      cjsuid,
+    };
+    const user: LoginUserDto = req.auth!;
+
+    const checkoutDB = await readCheckout(checkout, user);
+    if (!checkoutDB) {
+      res.status(404).json({ message: 'Checkout not found' });
+      return;
+    }
+
+    res.status(200).json(checkoutDB);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const searchCheckouts: RequestHandler = async (req, res, next) => {
+  try {
+    const pageQuery = req.query.page;
+    const pageSizeQuery = req.query.pageSize;
+
+    const page = Number(pageQuery);
+    const pageSize = Number(pageSizeQuery);
+
+    const params: FindCheckoutsParamsDto = {
+      page,
+      pageSize,
+    };
+    const user: LoginUserDto = req.auth!;
+    const checkoutsDB = await readCheckouts(params, user);
+
+    res.status(200).json(checkoutsDB);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateCheckoutStatus: RequestHandler = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    if (typeof id !== 'string') {
+      res.status(400).json({ message: 'Checkout id is required' });
+      return;
+    }
+
+    const body: UpdateCheckoutRequestBody = req.body;
+    const { cjestado } = body;
+    const checkout: UpdateCheckoutDto = {
+      cjid: id,
+      cjestado,
+    };
+
+    const user: LoginUserDto = req.auth!;
+    const updatedCheckout = await updateCheckout(checkout, user);
+
+    if (!updatedCheckout) {
+      res.status(404).json({ message: 'Checkout not found' });
+      return;
+    }
+
+    res.status(200).json(updatedCheckout);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { registerCheckout, searchCheckout, searchCheckouts, updateCheckoutStatus };
