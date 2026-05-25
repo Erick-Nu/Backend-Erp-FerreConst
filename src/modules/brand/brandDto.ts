1fcb60b24b936ff174f985e192f3b303bcb10dd9
Erick-Nu <erick.nunez02@epn.edu.ec>
2026-05-25 09:23:23 -0500

    Feature: Agregar archivo brandDto.ts en la capa dto del modulo brand

---
+++ src/modules/brand/brandDto.ts
@@ -0,0 +1,46 @@
+import type { Status } from '../../config/databaseTypes.js';
+
+type CreateBrandDto = {
+  mrcemid: string;
+  mrcnombre: string;
+};
+
+type FindBrandDto = {
+  mrcid: string;
+};
+
+type UpdateBrandDto = {
+  mrcid: string;
+  mrcnombre?: string;
+  mrcestado?: Status;
+};
+
+type FindBrandsParamsDto = {
+  page: number;
+  pageSize: number;
+};
+
+type BrandResponseDto = {
+  mrcid: string;
+  mrcemid: string;
+  mrcnombre: string;
+  mrcfchregistro: Date;
+  mrcestado: Status;
+};
+
+type FindBrandsResponseDto = {
+  items: BrandResponseDto[];
+  page: number;
+  pageSize: number;
+  totalItems: number;
+  totalPages: number;
+};
+
+export type {
+  CreateBrandDto,
+  FindBrandDto,
+  UpdateBrandDto,
+  FindBrandsParamsDto,
+  BrandResponseDto,
+  FindBrandsResponseDto,
+};
