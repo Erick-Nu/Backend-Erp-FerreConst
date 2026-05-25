80cc907c0ce1fa7aac34b731573ff996676d3af2
Erick-Nu <erick.nunez02@epn.edu.ec>
2026-05-25 09:22:57 -0500

    Feature: Agregar tipados para operaciones de base de datos

---
+++ src/config/databaseTypes.ts
@@ -0,0 +1,9 @@
+type Status = 'activo' | 'inactivo' | 'eliminado';
+
+type Role = 'administrador' | 'jefe' | 'empleado';
+
+type ProformaStatus = 'emitida' | 'pagada' | 'anulada';
+
+type Identification = 'cedula' | 'ruc';
+
+export type { Identification, ProformaStatus, Role, Status };
