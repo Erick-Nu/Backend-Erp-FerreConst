36ba99341b6b5d7f199541427880a3f86cc07227
Erick-Nu <erick.nunez02@epn.edu.ec>
2026-05-25 09:23:25 -0500

    Feature: Agregar archivo detalleProformaModel.ts en la capa model del modulo proforma

---
+++ src/modules/proforma/detalleProformaModel.ts
@@ -0,0 +1,12 @@
+type DetalleProformaModel = {
+  dprfmaid: string;
+  dprfmaprfmaid: string;
+  dprfmaesinventariable: boolean;
+  dprfmacodigo: string | null;
+  dprfmadescripcion: string;
+  dprfmacantidad: number;
+  dprfmapreciounitario: number;
+  dprfmapreciototal: number;
+};
+
+export type { DetalleProformaModel };
