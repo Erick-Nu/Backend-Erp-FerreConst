5cca872f4e0532e18d0d61a009587f61ab984ed2
Erick-Nu <erick.nunez02@epn.edu.ec>
2026-05-25 09:23:22 -0500

    Feature: Agregar archivo branchDto.ts en la capa dto del modulo branch

---
+++ src/modules/branch/branchDto.ts
@@ -0,0 +1,55 @@
+import type { Status } from '../../config/databaseTypes.js';
+
+type CreateBranchDto = {
+  suemid: string;
+  sunombre: string;
+  sudireccion: string | null;
+  sucorreo: string | null;
+  suidentificador: string;
+}
+
+type BranchResponseDto = {
+  suid: string;
+  suemid: string;
+  sunombre: string;
+  sudireccion: string | null;
+  sucorreo: string | null;
+  suidentificador: string;
+  sufchregistro: Date;
+  suestado: Status;
+}
+
+type FindBranchDto = {
+  suid: string;
+}
+
+type UpdateBranchDto = {
+  suid: string;
+  sunombre?: string;
+  sudireccion?: string | null;
+  sucorreo?: string | null;
+  suidentificador?: string;
+  suestado?: Status;
+}
+
+type FindBranchesParamsDto = {
+  page: number;
+  pageSize: number;
+};
+
+type FindBranchesResponseDto = {
+  items: BranchResponseDto[];
+  page: number;
+  pageSize: number;
+  totalItems: number;
+  totalPages: number;
+};
+
+export type {
+  CreateBranchDto,
+  BranchResponseDto,
+  FindBranchDto,
+  UpdateBranchDto,
+  FindBranchesParamsDto,
+  FindBranchesResponseDto,
+};
