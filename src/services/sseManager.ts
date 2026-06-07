153de61306740710d5f0afbc4d4a46607b5ad5ba
Erick-Nu <erick.nunez02@epn.edu.ec>
2026-06-06 20:11:06 -0500

    Feature: Agregar servicio SSE para eventos en tiempo real

---
+++ src/services/sseManager.ts
@@ -0,0 +1,29 @@
+import type { Response } from 'express';
+import { logger } from '../utils/logger.js';
+
+const SSE_KEEPALIVE_INTERVAL_MS = 15000;
+
+function sendSSEData(res: Response, event: string, data: object): void {
+  res.write(`event: ${event}\ndata: ${JSON.stringify(data)}\n\n`);
+}
+
+function createSSEConnection(res: Response, onDisconnect: () => void): void {
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    Connection: 'keep-alive',
+  });
+
+  res.write('event: connected\ndata: {}\n\n');
+
+  const keepAliveId = setInterval(() => {
+    res.write(':keepalive\n\n');
+  }, SSE_KEEPALIVE_INTERVAL_MS);
+
+  res.on('close', () => {
+    clearInterval(keepAliveId);
+    onDisconnect();
+  });
+}
+
+export { createSSEConnection, sendSSEData };
